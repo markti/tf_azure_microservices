89180585c2d97d5e77dada2ed4a83589c37217af
Mark Tinderholt <mark@vitalsigyn.com>
2020-06-06 08:54:34 -0400

    eg v3

---
+++ eventgrid_v3/variables.tf
@@ -0,0 +1,28 @@
+variable "resource_group_name" { }
+variable "location" { }
+variable "name" { }
+variable "app_name" { }
+variable "env_name" { }
+variable "service_name" { }
+
+variable "azure_function_version" { }
+variable "worker_runtime" { }
+variable "app_settings" {
+    description = "An array of properties to pass into the Azure Function for its app settings collection"
+}
+
+variable "host_settings" {
+  type = object({
+
+    plan_id = string
+    storage_connection_string = string
+    instrumentation_key = string
+    keyvault_id = string
+    deployment_storage_account_name = string
+    deployment_storage_container = string
+    deployment_package_sas = string
+
+  })
+}
+variable "deployment_package_filename" { }
+variable "loganalytics_workspace_id" { }
