fbd8ee1ecb7f14a8b6af83954de64af3a7c8fcd5
Mark Tinderholt <mark@vitalsigyn.com>
2020-06-05 22:04:03 -0400

    update host_settings var for base_v2

--- http/base_v2/variables.tf
+++ http/base_v2/variables.tf
@@ -12,9 +12,9 @@ variable "host_settings" {
     storage_connection_string = string
     instrumentation_key = string
     keyvault_id = string
-    deployment_storage_account_name = string
-    deployment_storage_container = string
-    deployment_package_sas = string
+    code_storage_account_name = string
+    code_storage_container = string
+    code_package_sas = string
 
   })
 }
