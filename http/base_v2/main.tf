829ec306c0a507ede859be4ca2f75c0a9f8c301d
Mark Tinderholt <mark@vitalsigyn.com>
2020-06-05 22:02:18 -0400

    update base v2 with host_settings object

--- http/base_v2/main.tf
+++ http/base_v2/main.tf
@@ -4,7 +4,7 @@
 locals {
 
   required_settings = {
-      "APPINSIGHTS_INSTRUMENTATIONKEY"    = var.instrumentation_key,
+      "APPINSIGHTS_INSTRUMENTATIONKEY"    = var.host_settings.instrumentation_key,
       "WEBSITE_RUN_FROM_PACKAGE"          = local.code_drop_url,
       "ClientID"                          = module.service_principal.client_id,
       "ClientSecret"                      = module.service_principal.client_secret
@@ -23,8 +23,8 @@ module "api_fn" {
   name                          = var.name
   resource_group_name           = var.resource_group_name
   location                      = var.location
-  app_service_plan_id           = var.hosting_plan_id
-  storage_connection_string     = var.hosting_storage_connection_string
+  app_service_plan_id           = var.host_settings.plan_id
+  storage_connection_string     = var.host_settings.storage_connection_string
   azure_function_version        = var.azure_function_version
   worker_runtime                = var.worker_runtime
   app_settings                  = local.combined_settings
