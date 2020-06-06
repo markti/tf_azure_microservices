5da4cfdcc27f835c2cd51aa90ac57905833b05b3
Mark Tinderholt <mark@vitalsigyn.com>
2020-06-05 21:53:03 -0400

    output host settings

--- host/premium/outputs.tf
+++ host/premium/outputs.tf
@@ -22,3 +22,17 @@ output "code_storage_container" {
 output "code_storage_sas" {
   value = data.azurerm_storage_account_sas.code_storage.sas
 }
+
+output "host_settings" {
+  value = object({
+    hosting_plan_id           = module.api_hosting_plan.id
+    storage_connection_string = module.api_hosting_plan.storage_connection_string
+    instrumentation_key       = module.appinsights.instrumentation_key
+    keyvault_id               = module.keyvault.id
+    keyvault_uri              = module.keyvault.uri
+    code_storage_account_name = module.code_storage.name
+    code_storage_container    = azurerm_storage_container.code_storage.name
+    code_storage_sas          = data.azurerm_storage_account_sas.code_storage.sas
+
+  })
+}
