10aa9ce7df7ab5cb2c3649794e0cec90cb71f672
Mark <Mark@USL008374>
2020-06-30 21:27:28 -0400

    6-30-2020 version

---
+++ host/premium_v2020-06-30/variables.tf
@@ -0,0 +1,22 @@
+variable "environment" {
+  type = object({
+
+    app_name = string
+    env_name = string
+    resource_group_name = string
+    location = string
+    location_suffix = string
+
+  })
+}
+variable "name" { }
+variable "storage_type" { 
+    default = "GRS" 
+}
+variable "workspace_id" { }
+variable "maximum_instance_count" {
+  default = 1
+}
+variable "minimum_instance_count" {
+  default = 1
+}
